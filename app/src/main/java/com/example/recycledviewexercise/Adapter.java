6e683b0f3fce162ebe92ad6c84e854eed01bbc93
MateuszFryda <mateuszfryda@gmail.com>
2019-03-15 02:22:55 +0100

    Minor changes in the adapter class and layout

--- app/src/main/java/com/example/recycledviewexercise/Adapter.java
+++ app/src/main/java/com/example/recycledviewexercise/Adapter.java
@@ -1,18 +1,26 @@
 package com.example.recycledviewexercise;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     private List<ModelClass> modelClassList;
+    Context mContext;
 
     public Adapter(List<ModelClass> modelClassList) {
         this.modelClassList = modelClassList;
@@ -26,11 +34,19 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-        int resource = modelClassList.get(position).getImageResource();
-        String title = modelClassList.get(position).getTitle();
-        String body = modelClassList.get(position).getBody();
-        viewHolder.setData(resource,title,body);
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
+        final int resource = modelClassList.get(position).getImageResource();
+        final String title = modelClassList.get(position).getTitle();
+        final String body = modelClassList.get(position).getBody();
+        Log.d(TAG, "onBindViewHolder: called.");
+
+//        Glide.with(mContext)
+//                .asBitmap()
+//                .load(mImageUrls.get(position))
+//                .into(holder.image);
+        viewHolder.setData(resource, title, body);
+
+
     }
 
     @Override
